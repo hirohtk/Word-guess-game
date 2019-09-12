b4c69c725ca213a5df4fd15275e8d3d8d6157610
Kensen <hirohtk@gmail.com>
2019-09-11 19:34:33 -0700

    working game for one type of sushi, need to have user choice add to existing array with underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,186 +19,189 @@ var ebi = "ebi"
 
 var sushiArray = ["maguro", "sake", "unagi", "ebi"]
 
+initialize();
 
-var computerChoiceMath = Math.floor(Math.random() * 0); // setting this to zero for now so it always chooses maguro
-var computerChoice = sushiArray[computerChoiceMath];
-console.log(computerChoice);
-
-console.log(computerChoice.indexOf("m"));
-
-
-if (computerChoice = "maguro") {
-    maguroGame();
-}
-
-
-function gameRefresh() {
-    var computerChoiceMath = Math.floor(Math.random() * 0);
+function initialize() {
+    
+    var computerChoiceMath = Math.floor(Math.random() * 0); // setting this to zero for now so it always chooses maguro
     var computerChoice = sushiArray[computerChoiceMath];
     console.log(computerChoice);
 
+    console.log(computerChoice.indexOf("m"));
+
+
+    if (computerChoice = "maguro") {
+        guessesLeft = 10;
+        maguroGame();
+    }
+
+
+}
+
+function gameRefresh() {
+    var userChoiceArrayWriter = document.getElementById("userChoiceArray"); //created an array for user selection
+    userChoiceArrayWriter.textContent = "";
+
     userChoiceArray.length = 0;
-    userChoiceArrayDisplay.length - 0;
-    guessesLeft = 10;
+    userChoiceArrayDisplay.length = 0;
+    userChoiceArrayBTS.length = 0;
+    
+    
+    initialize();
 
 }
 
 
-    function arraysMatch (arr1, arr2) {
+function arraysMatch(arr1, arr2) {
 
-        // Check if the arrays are the same length
-        if (arr1.length !== arr2.length) return false;
-    
-        // Check if all items exist and are in the same order
-        for (var i = 0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) return false;
-        }
-    
-        // Otherwise, return true
-        return true;
-    
+    // Check if the arrays are the same length
+    if (arr1.length !== arr2.length) return false;
+
+    // Check if all items exist and are in the same order
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
     }
 
-    function victory() {
-        wins++;
-        var winsWriter = document.getElementById("victories");
-        winsWriter.textContent = wins;
-    }
-    
+    // Otherwise, return true
+    return true;
 
-    function maguroGame() {
+}
 
-        var currentWordWriter = document.getElementById("currentWord");
-        currentWordWriter.textContent = "_ _ _ _ _ _";
+function victory() {
+    wins++;
+    var winsWriter = document.getElementById("victories");
+    winsWriter.textContent = wins;
+    gameRefresh();
+    initialize();
+}
 
-        document.onkeyup = function (event) {
 
-            var userChoice = event.key;
+function maguroGame() {
 
+    var currentWordWriter = document.getElementById("currentWord");
+    currentWordWriter.textContent = userChoiceArrayBTS;
+
+    document.onkeyup = function (event) {
+
+        var userChoice = event.key;
+
+
+
+        if (alphabet.includes(userChoice)) { // If my array alphabet includes my guess, then
+            
             var userChoiceArrayWriter = document.getElementById("userChoiceArray"); //created an array for user selection
             userChoiceArray.push(userChoice); //pushing userChoice from above onto the end of an array
             userChoiceArrayWriter.textContent = userChoiceArray;
-
-            if (alphabet.includes(userChoice)) { // If my array alphabet includes my guess, then
-                invalidEntryWriter = document.getElementById("invalidEntry");
-                invalidEntryWriter.textContent = ""; // so if you DO put a valid letter, string with nothing writes into the span
-                step1();  //enters the game 
-            }
-
-            else {
-                invalidEntryWriter = document.getElementById("invalidEntry");
-                invalidEntryWriter.textContent = invalidEntry;  // so if you DON'T put a valid letter, string with "Invalid Entry" writes into the span
-            }
-
-            function clearChoices() {
-                var userChoiceArrayWriter = document.getElementById("userChoiceArray"); //created an array for user selection
-                userChoiceArrayWriter.textContent = "";
-                
-            }
-
-            
-
-            function step1() {
-                if (maguro.includes(userChoice) && guessesLeft !== 0) {
-                    step2();
-                }
-                else if (guessesLeft === 0) {
-                    losses++;
-                    gameRefresh();
-                    clearChoices();
-                }
-                else {
-                    guessesLeft--;
-                }
-
-                var guessLeftWriter = document.getElementById("guessesLeft");
-                guessLeftWriter.textContent = guessesLeft
-
-
-
-                var lossesWriter = document.getElementById("defeats");
-                lossesWriter.textContent = losses;
-            }
-
-            function step2() {
-                userChoiceArrayDisplay.length = 6;
-                if (userChoice === "m") {
-                    userChoiceArrayDisplay[0] = "m";
-                    console.log(userChoiceArrayDisplay);
-                    if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
-                        victory();
-                        gameRefresh();
-                        clearChoices();
-                    }
-                    
-
-                }
-                else if (userChoice === "a") {
-                    userChoiceArrayDisplay[1] = "a";
-                    console.log(userChoiceArrayDisplay);
-                    if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
-                        victory();
-                        gameRefresh();
-                        clearChoices();
-                    }
-                    
-
-                }
-                else if (userChoice === "g") {
-                    userChoiceArrayDisplay[2] = "g";
-                    console.log(userChoiceArrayDisplay);
-                    if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
-                        victory();
-                        gameRefresh();
-                        clearChoices();
-                    }
-                    
-
-                }
-                else if (userChoice === "u") {
-                    userChoiceArrayDisplay[3] = "u";
-                    console.log(userChoiceArrayDisplay);
-                    if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
-                        victory();
-                        gameRefresh();
-                        clearChoices();
-                    }
-                   
-
-                }
-                else if (userChoice === "r") {
-                    userChoiceArrayDisplay[4] = "r";
-                    console.log(userChoiceArrayDisplay);
-                    if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
-                        victory();
-                        gameRefresh();
-                        clearChoices();
-                    }
-                   
-
-                }
-                else if (userChoice === "o") {
-                    userChoiceArrayDisplay[5] = "o";
-                    console.log(userChoiceArrayDisplay);
-                    if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
-                        victory();
-                        gameRefresh();
-                        clearChoices();
-                    }
-                    
-                }
-
-                var currentWordWriter = document.getElementById("currentWord");
-                currentWordWriter.textContent = userChoiceArrayDisplay;
-
-
-
-
-
-
-
-
-            }
+            step1();  //enters the game 
         }
 
+        else {
+            invalidEntryWriter = document.getElementById("invalidEntry");
+            invalidEntryWriter.textContent = invalidEntry;  // so if you DON'T put a valid letter, string with "Invalid Entry" writes into the span
+        }
+
+        function clearChoices() {
+            var userChoiceArrayWriter = document.getElementById("userChoiceArray"); //created an array for user selection
+            userChoiceArrayWriter.textContent = "";
+
+        }
+
+
+
+        function step1() {
+            if (maguro.includes(userChoice) && guessesLeft !== 0) {
+                step2();
+            }
+            else if (guessesLeft === 0) {
+                losses++;
+                gameRefresh();
+                clearChoices();
+            }
+            else {
+                guessesLeft--;
+            }
+
+            var guessLeftWriter = document.getElementById("guessesLeft");
+            guessLeftWriter.textContent = guessesLeft
+
+
+
+            var lossesWriter = document.getElementById("defeats");
+            lossesWriter.textContent = losses;
+        }
+
+        function step2() {
+            userChoiceArrayDisplay.length = 6;
+            if (userChoice === "m") {
+                userChoiceArrayDisplay[0] = "m";
+                console.log(userChoiceArrayDisplay);
+                if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
+                    victory();
+                    gameRefresh();
+
+                }
+
+
+            }
+            else if (userChoice === "a") {
+                userChoiceArrayDisplay[1] = "a";
+                console.log(userChoiceArrayDisplay);
+                if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
+                    victory();
+                    gameRefresh();
+
+                }
+
+
+            }
+            else if (userChoice === "g") {
+                userChoiceArrayDisplay[2] = "g";
+                console.log(userChoiceArrayDisplay);
+                if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
+                    victory();
+                    gameRefresh();
+
+                }
+
+
+            }
+            else if (userChoice === "u") {
+                userChoiceArrayDisplay[3] = "u";
+                console.log(userChoiceArrayDisplay);
+                if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
+                    victory();
+                    gameRefresh();
+
+                }
+
+
+            }
+            else if (userChoice === "r") {
+                userChoiceArrayDisplay[4] = "r";
+                console.log(userChoiceArrayDisplay);
+                if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
+                    victory();
+                    gameRefresh();
+
+                }
+
+
+            }
+            else if (userChoice === "o") {
+                userChoiceArrayDisplay[5] = "o";
+                console.log(userChoiceArrayDisplay);
+                if ((arraysMatch(userChoiceArrayDisplay, maguroArray))) {
+                    victory();
+                    gameRefresh();
+
+                }
+
+            }
+
+            var currentWordWriter = document.getElementById("currentWord");
+            currentWordWriter.textContent = userChoiceArrayDisplay;
+
+        }
     }
+
+}
